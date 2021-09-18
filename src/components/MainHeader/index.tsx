f54620f0e96e554e1e68e2567904cae310c3b630
luisgbyte <44074498+luisgbyte@users.noreply.github.com>
2021-09-18 14:56:17 -0300

    create Header

--- src/components/MainHeader/index.tsx
+++ src/components/MainHeader/index.tsx
@@ -1,11 +1,23 @@
-import React from 'react';
+import React, { useMemo } from 'react';
 
-import { Container } from './styles';
+import emojis from '../../utils/emojis';
+
+import { Container, Profile, Welcome, UserName} from './styles';
 
 const MainHeader: React.FC = () => {
+
+  const emoji = useMemo(() => {
+    const indice = Math.floor(Math.random() * emojis.length);
+    return emojis[indice]
+  }, [])
+
   return (
     <Container>
       <h1>MainHeader</h1>
+      <Profile>
+        <Welcome>Hello, {emoji}</Welcome>
+        <UserName>Guest</UserName>
+      </Profile>
     </Container>
   );
 }
