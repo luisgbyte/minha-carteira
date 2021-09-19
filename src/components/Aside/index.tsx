14dc7907ccd5fec252327cee22c10004efb6edd6
luisgbyte <44074498+luisgbyte@users.noreply.github.com>
2021-09-19 19:19:20 -0300

    create component Aside

--- src/components/Aside/index.tsx
+++ src/components/Aside/index.tsx
@@ -1,13 +1,55 @@
-import React from 'react';
+import React from "react";
 
-import { Container } from './styles';
+import {
+  MdDashboard,
+  MdArrowDownward,
+  MdArrowUpward,
+  MdExitToApp,
+} from "react-icons/md";
+
+import logoImg from "../../assets/Logo.svg";
+
+import {
+  Container,
+  Header,
+  Title,
+  LogoImg,
+  MenuContainer,
+  MenuItemLink,
+} from "./styles";
 
 const Aside: React.FC = () => {
   return (
     <Container>
-      <h1>Aside</h1>
+      <Header>
+        <LogoImg src={logoImg} alt="Logo minha carteira" />
+        <Title>Minha Carteira</Title>
+      </Header>
+      <MenuContainer>
+
+        <MenuItemLink href="#">
+          <MdDashboard />
+          Dashboard
+        </MenuItemLink>
+
+        <MenuItemLink href="#">
+          <MdArrowUpward />
+          Entradas
+        </MenuItemLink>
+
+        <MenuItemLink href="#">
+          <MdArrowDownward />
+          Saídas
+        </MenuItemLink>
+
+        <MenuItemLink href="#">
+          <MdExitToApp />
+          Sair
+        </MenuItemLink>
+        
+      </MenuContainer>
     </Container>
   );
-}
+};
 
 export default Aside;
