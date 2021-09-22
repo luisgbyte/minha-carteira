a90149f4698730a35d6068b2fedf7faf3098fe76
luisgbyte <44074498+luisgbyte@users.noreply.github.com>
2021-09-22 10:30:18 -0300

    creating page navigation

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,20 @@
 import React from "react";
+
 import { ThemeProvider } from "styled-components";
 import GlobalStyles from "./styles/GlobalStyles";
 
-import Layout from "./components/Layout";
-// import Dashboard from "./pages/Dashboard";
-import List from "./pages/List";
+// import Layout from "./components/Layout";
 import dark from "./styles/themes/dark";
+
+import Routes from "./routes";
 
 const src: React.FC = () => {
   return (
     <ThemeProvider theme={dark}>
       <GlobalStyles />
-      <Layout>
-        {/* <Dashboard /> */}
-        <List />
-      </Layout>
+      {/* <Layout> */}
+        <Routes/>
+      {/* </Layout> */}
     </ThemeProvider>
   );
 };
