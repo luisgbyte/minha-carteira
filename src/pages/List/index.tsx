f86951e044fb3a586ca6f511b6bd503565e5104c
luisgbyte <44074498+luisgbyte@users.noreply.github.com>
2021-09-22 09:28:39 -0300

    creating the filters

--- src/pages/List/index.tsx
+++ src/pages/List/index.tsx
@@ -1,24 +1,40 @@
 import React from "react";
 
-import { Container, Content } from "./styles";
+import { Container, Content, Filters } from "./styles";
 
 import ContentHeader from "../../components/ContentHeader";
 import SelectInput from "../../components/SelectInput";
 import HistoryFinanceCard from "../../components/HistoryFinanceCard";
 
 const List: React.FC = () => {
-  const options = [
-    {
-      value: "teste",
-      label: "teste",
-    },
+  const months = [
+    { value: 7, label: "Julho" },
+    { value: 8, label: "Agosto" },
+    { value: 9, label: "Setembro" },
+  ];
+
+  const years = [
+    { value: 2020, label: 2020 },
+    { value: 2019, label: 2019 },
+    { value: 2018, label: 2018 },
   ];
 
   return (
     <Container>
       <ContentHeader title="Saídas" lineColor="#e44c4e">
-        <SelectInput options={options} />
+        <SelectInput options={months} />
+        <SelectInput options={years} />
       </ContentHeader>
+
+      <Filters>
+        <button type="button" className="tag-filter tag-filter-recurrent">
+          Recorrentes
+        </button>
+        <button type="button" className="tag-filter tag-filter-eventual">
+          Eventuais
+        </button>
+      </Filters>
+
       <Content>
         <HistoryFinanceCard
           tagColor="#e44c4e"
