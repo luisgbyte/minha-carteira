b0bf0a0e921a58e9fc54f23a034960fca349f491
luisgbyte <44074498+luisgbyte@users.noreply.github.com>
2021-09-24 12:03:04 -0300

    listing occurrences and entries

--- src/pages/List/index.tsx
+++ src/pages/List/index.tsx
@@ -1,10 +1,13 @@
-import React, { useMemo } from "react";
+import React, { useMemo, useState, useEffect } from "react";
 
 import { Container, Content, Filters } from "./styles";
 
 import ContentHeader from "../../components/ContentHeader";
 import SelectInput from "../../components/SelectInput";
 import HistoryFinanceCard from "../../components/HistoryFinanceCard";
+
+import gains from "../../repositories/gains";
+import expenses from "../../repositories/expenses";
 
 interface IRouteParams {
   match: {
@@ -14,15 +17,30 @@ interface IRouteParams {
   };
 }
 
+interface IData {
+  id: string;
+  description: string;
+  amountFormatted: string;
+  frequency: string;
+  dataFormatted: string;
+  tagColor: string;
+}
+
 const List: React.FC<IRouteParams> = ({ match }) => {
+  const [data, setData] = useState<IData[]>([]);
+
   const { type } = match.params;
 
   const title = useMemo(() => {
-    return type === "entry-balance" ? 'Entradas' : 'Saídas';
+    return type === "entry-balance" ? "Entradas" : "Saídas";
   }, [type]);
 
   const lineColor = useMemo(() => {
-    return type === "entry-balance" ? '#f7931b' : '#e44c4e';
+    return type === "entry-balance" ? "#f7931b" : "#e44c4e";
+  }, [type]);
+
+  const listData = useMemo(() => {
+    return type === "entry-balance" ? gains : expenses;
   }, [type]);
 
   const months = [
@@ -36,6 +54,21 @@ const List: React.FC<IRouteParams> = ({ match }) => {
     { value: 2019, label: 2019 },
     { value: 2018, label: 2018 },
   ];
+
+  useEffect(() => {
+    const response = listData.map((item) => {
+      return {
+        id: String(Math.random() * data.length),
+        description: item.description,
+        amountFormatted: item.amount,
+        frequency: item.frequency,
+        dataFormatted: item.date,
+        tagColor: item.frequency === "recorrente" ? "#4e41f0" : "#e44c4e",
+      };
+    });
+
+    setData(response);
+  }, [listData, data.length]);
 
   return (
     <Container>
@@ -54,12 +87,15 @@ const List: React.FC<IRouteParams> = ({ match }) => {
       </Filters>
 
       <Content>
-        <HistoryFinanceCard
-          tagColor="#e44c4e"
-          title="Conta de Luz"
-          subtitle="20/08/2021"
-          amount="R$ 130,00"
-        />
+        {data.map((item) => (
+          <HistoryFinanceCard
+            key={item.id}
+            tagColor={item.tagColor}
+            title={item.description}
+            subtitle={item.dataFormatted}
+            amount={item.amountFormatted}
+          />
+        ))}
       </Content>
     </Container>
   );
