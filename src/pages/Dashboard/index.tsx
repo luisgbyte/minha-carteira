8e8daf8f8fc09929d45cfa72294c05ec244fad58
luisgbyte <44074498+luisgbyte@users.noreply.github.com>
2021-09-30 16:14:09 -0300

    loading the pie chart with data

--- src/pages/Dashboard/index.tsx
+++ src/pages/Dashboard/index.tsx
@@ -4,7 +4,7 @@ import ContentHeader from "../../components/ContentHeader";
 import SelectInput from "../../components/SelectInput";
 import WalletBox from "../../components/WalletBox";
 import MessageBox from "../../components/MessageBox";
-import PieChart from "../../components/PieChart";
+import PieChartBox from "../../components/PieChartBox";
 
 import expenses from "../../repositories/expenses";
 import gains from "../../repositories/gains";
@@ -123,6 +123,30 @@ const Dashboard: React.FC = () => {
     }
   }, [totalBalance]);
 
+  const relationExpensesVersusGains = useMemo(() => {
+    const total = totalGains + totalExpenses; 
+  
+    const percentGains = (totalGains / total) * 100;
+    const percentExpenses = (totalExpenses / total) * 100;
+
+    const data = [
+      {
+        name: "Entradas",
+        value: totalExpenses,
+        percent: Number(percentGains.toFixed(1)),
+        color: '#e44c4e'
+      },
+      {
+        name: "Saídas",
+        value: totalExpenses,
+        percent: Number(percentExpenses.toFixed(1)),
+        color: '#f7931b'
+      }
+    ];
+
+    return data;
+  },[totalGains, totalExpenses]);
+
   const handleMonthSelected = (month: string) => {
     try {
       const parseMonth = Number(month);
@@ -186,7 +210,7 @@ const Dashboard: React.FC = () => {
           icon={message.icon}
         />
 
-        <PieChart/>
+        <PieChartBox data={relationExpensesVersusGains} />
         
       </Content>
     </Container>
