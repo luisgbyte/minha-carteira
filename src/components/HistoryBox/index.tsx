ab22109268351370a1dcd5acc4df34dbf3f5a21f
luisgbyte <44074498+luisgbyte@users.noreply.github.com>
2021-10-01 08:10:31 -0300

    formatting balance history graph legend values

--- src/components/HistoryBox/index.tsx
+++ src/components/HistoryBox/index.tsx
@@ -9,6 +9,8 @@ import {
   Tooltip,
 } from "recharts";
 
+import formatCurrency from '../../utils/formatCurrency';
+
 import {
   Container,
   ChartContainer,
@@ -57,7 +59,7 @@ const HistoryBox: React.FC<IHistoryBoxProps> = ({
         >
           <CartesianGrid strokeDasharray="3 3" stroke="#cecece" />
           <XAxis dataKey="month" stroke="#cecece" />
-          <Tooltip />
+          <Tooltip formatter={(value: number) => formatCurrency(Number(value))} />
           <Line
             type="monotone"
             dataKey="amountEntry"
