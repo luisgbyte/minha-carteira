bb3c8e2d3f9f480fc19fde0fc6359bb5e7562ace
luisgbyte <44074498+luisgbyte@users.noreply.github.com>
2021-10-03 11:49:16 -0300

    creating the authentication hooks

--- src/pages/SignIn/index.tsx
+++ src/pages/SignIn/index.tsx
@@ -1,13 +1,20 @@
-import React from "react";
+import React, { useState } from "react";
 
 import logoImg from "../../assets/Logo.svg";
 
 import Input from "../../components/Input";
 import Button from "../../components/Button";
 
+import { useAuth } from '../../hooks/auth';
+
 import { Container, Logo, Form, FormTitle } from "./styles";
 
 const SignIn: React.FC = () => {
+  const [email, setEmail] = useState<string>('');
+  const [password, setPassword] = useState<string>('');
+
+  const { signIn } = useAuth();
+
   return (
     <Container>
       <Logo>
@@ -15,11 +22,21 @@ const SignIn: React.FC = () => {
         <h2>Minha Carteira</h2>
       </Logo>
 
-      <Form onSubmit={() => {}}>
+      <Form onSubmit={() => signIn(email, password)}>
         <FormTitle>Entrar</FormTitle>
 
-        <Input type="email" required placeholder="e-mail" />
-        <Input type="password" required placeholder="senha" />
+        <Input
+          type="email"
+          required
+          placeholder="e-mail"
+          onChange={(e) => setEmail(e.target.value)}
+        />
+        <Input
+          type="password"
+          required
+          placeholder="senha"
+          onChange={(e) => setPassword(e.target.value)}
+        />
 
         <Button type="submit">Acessar</Button>
       </Form>
